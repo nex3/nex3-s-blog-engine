51c35eaa033b10566f1a846caef25dcbdfec21da
Nathan Weizenbaum <nex342@gmail.com>
2007-09-17 22:24:07 +0000

    Add a spec for nameless tag non-validation.

--- spec/models/tag.rb
+++ spec/models/tag.rb
@@ -1,9 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Tag, "with a couple posts" do
+describe Tag, " with a couple posts" do
   fixtures :posts, :posts_tags, :tags
 
   it "should have the proper two posts" do
     tags(:emacs).posts.find(:all, :order => 'title DESC').should == [posts(:commented), posts(:first)]
+  end
+end
+
+describe Tag, " without a name" do
+  fixtures :tags
+
+  it "should have one error on name" do
+    tags(:nameless).should have(1).error_on(:title)
   end
 end
